3c479ce9aecbdc779fefc4c13200072210f447fd
arisromil <arisromil@yahoo.com>
2024-02-07 09:13:38 -0700

    Create Failure.java

---
+++ src/main/java/exceptions/Failure.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+public record Failure<T>(Throwable throwable) implements Try<T> {
+    @Override
+    public T getResult() { throw new RuntimeException("Invalid invocation"); }
+
+    @Override
+    public Throwable getError() { return throwable; }
+}
